f045b5d5cc72347ed5dcd643ed0cef94dfccee43
Matthew <mattj20@gmail.com>
2022-12-25 20:36:55 -0800

    commit tree 5cf6bdf1ca06cd2f73e1de96380d15d4f87b816c

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './App.css';
+import { addTodo } from  './actions/todo';
 
 class App extends Component {
 
@@ -17,11 +18,12 @@ class App extends Component {
   handleOnSubmit = event => {
     event.preventDefault();
     console.log("Todo being added: ", this.state.todo);
-    this.props.dispatch({ type: 'ADD_TODO', todo: this.state.todo });
+    this.props.addTodo(this.state.todo);
     this.setState({ todo: '' });
   }
 
   render() {
+    // debugger
     const renderTodos = () => this.props.todos.map(todo => <li key={todo}>{todo}</li>);
     return (
       <div className="App">
@@ -41,10 +43,20 @@ class App extends Component {
   }
 };
 
-const mapStateToProps = (state) => {
-  return {
-    todos: state.todos
-  };
-};
+// const mapStateToProps = (state) => {
+//   return {
+//     todos: state.todos
+//   };
+// };
 
-export default connect(mapStateToProps)(App);
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     addTodo: (todo) => {
+//       dispatch(addTodo(todo))
+//     }
+//   };
+// };
+
+// export default connect(mapStateToProps, mapDispatchToProps)(App);
+
+export default connect(state => ({ todos: state.todos }), { addTodo })(App);
